61c76855340215a2b3369f49053eeff33ecf0c89
heather995 <137695754+heather995@users.noreply.github.com>
2023-08-15 12:06:37 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const accordionHeaders = document.querySelectorAll('.accordion-header');
+
+    accordionHeaders.forEach(header => {
+        header.addEventListener('click', () => {
+            header.parentElement.classList.toggle('active');
+        });
+    });
+});
